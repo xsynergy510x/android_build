76d699b9243ba0ff06641f98ae1382e2c99b3b92
JustArchi <JustArchi@JustArchi.net>
2016-02-08 13:06:01 +0100

    No more ICEs please

--- core/archidroid.mk
+++ core/archidroid.mk
@@ -56,7 +56,7 @@ ARCHIDROID_GCC_CFLAGS += -fgraphite -fgraphite-identity
 # ARCHIDROID_GCC_CFLAGS += -floop-block -floop-interchange -floop-nest-optimize -floop-parallelize-all -floop-strip-mine
 
 # These flags have been disabled because of assembler errors
-ARCHIDROID_GCC_CFLAGS += -fmodulo-sched -fmodulo-sched-allow-regmoves
+# ARCHIDROID_GCC_CFLAGS += -fmodulo-sched -fmodulo-sched-allow-regmoves
 
 ####################
 ### MISC SECTION ###
