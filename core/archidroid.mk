f6fbcb13e6db1b4a03423b22b1e7f9ff49bb4ab3
Eduard Alfonso <xsynergy510x@gmail.com>
2016-03-05 16:30:42 -0800

    Archidroid: Disable GCC5.3 flags, enable graphite

--- core/archidroid.mk
+++ core/archidroid.mk
@@ -45,7 +45,7 @@ ARCHIDROID_GCC_LDFLAGS := -Wl,-O3 -Wl,--as-needed -Wl,--gc-sections -Wl,--relax 
 # Flags below are applied to specific targets only, use them if your flag is not compatible for both compilers
 
 # We use GCC 5.3 for arm-linux-androideabi, make sure to remove flags below if you decided to stick with 4.9
-ARCHIDROID_GCC_CFLAGS_32 := -Wno-error=bool-compare -Wno-error=logical-not-parentheses -Wno-error=sizeof-array-argument
+# ARCHIDROID_GCC_CFLAGS_32 := -Wno-error=bool-compare -Wno-error=logical-not-parentheses -Wno-error=sizeof-array-argument
 
 # We use GCC 4.9 for aarch64-linux-android, so we don't have any extra flags for it
 ARCHIDROID_GCC_CFLAGS_64 := 
@@ -64,7 +64,7 @@ ARCHIDROID_GCC_CFLAGS_64 :=
 # ARCHIDROID_GCC_CFLAGS += -ftracer -funroll-loops
 
 # These flags may cause ICEs in some compilers, but work fine in other ones, test carefully
-# ARCHIDROID_GCC_CFLAGS += -fgraphite -fgraphite-identity
+ARCHIDROID_GCC_CFLAGS += -fgraphite -fgraphite-identity
 
 # The following flags (-floop) require that your GCC has been configured --with-isl
 # Additionally, applying any of them will most likely cause ICE in your compiler, so they're disabled
